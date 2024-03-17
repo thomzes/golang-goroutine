57817fc9e489ff454341ab6c6aa555bcc95f2139
Thomzes <thomz161@gmail.com>
2024-03-18 00:06:19 +0700

    feat: create test func for go routine

---
+++ goroutine_test.go
@@ -0,0 +1,30 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoRoutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Ups")
+
+	time.Sleep(1 * time.Second)
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display ", number)
+}
+
+func TestManyGoRoutine(t *testing.T) {
+	for i := 1; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
