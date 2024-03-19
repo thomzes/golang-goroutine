d98ba62942181abde14ebf70063e42de978c6f6d
Tommy <thomz161@gmail.com>
2024-03-19 16:42:44 +0700

    feat: create timer test

---
+++ atomic_test.go
@@ -0,0 +1,29 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			group.Add(1)
+			for j := 0; j < 100; j++ {
+				// x = x + 1
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	// time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+
+}
