0c53cacfb345ada42089cf473d2d54f5f656330d
Tommy <thomz161@gmail.com>
2024-03-19 16:24:07 +0700

    feat: create func sync pool,map,once,cond

---
+++ pool_test.go
@@ -0,0 +1,33 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Thomas")
+	pool.Put("Ardiansah")
+	pool.Put("Dadang")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
+	fmt.Println("Selesai")
+
+}
